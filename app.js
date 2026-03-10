1f115f2b2db594704da27d28168ef8fefdd71cdb
Likhilesh <likbalpande@example.com>
2026-03-10 18:34:10 +0530

    basic server

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+
+app.get("/health", (req, res) => {
+    console.log("Health check hit");
+    res.json({ status: "ok" });
+});
+
+app.get("/hello", (req, res) => {
+    console.log("Hello endpoint hit");
+    console.log(`IP : ${req.headers["x-forwarded-for"] || req.socket.remoteAddress}`);
+    res.json({ message: "Hello, World!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`------- Server running on port ${PORT} ---------`);
+});
