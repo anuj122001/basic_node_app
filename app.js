d3296fb60e0715c1c5330056fe2a8f27e02c526e
Anuj Jain <anujjain122001@gmail.com>
2026-04-14 17:07:27 +0530

    some changes done

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
     console.log("App is running...");
-    res.json({ status: "App is running" });
+    res.json({ status: "App is running again and again" });
 });
 
 app.get("/health", (req, res) => {
